362d97813523913f4b5b501ca9b01ad9f1ba6628
Rehman Qureshi <rehman.qureshi@hpi.de>
2026-06-24 17:08:18 +0200

    Added load_data and driver files

--- driver.py
+++ driver.py
@@ -1,4 +1,51 @@
 
+import sys
+
+from load_data import load_data_function 
+
 if __name__ == "__main__":
-    # Load the dataset
-    print("Loading dataset...")
+
+    if len(sys.argv) != 3:
+        print("Usage: python driver.py <events_logs_path> <declarative_constraints_path>")
+        sys.exit(1)
+
+    events_logs_path = sys.argv[1]
+    declarative_constraints_path = sys.argv[2]
+
+    # Example usage
+    # events_logs_path = "BPIC19_3way_IbeforeGR_standardPO_complete.xes"
+    # declarative_constraints_path = "declarative_constraints.json"
+
+    event_logs, declarative_constraints = load_data_function(events_logs_path, declarative_constraints_path)
+
+    #print("Event Logs:")
+    #print(event_logs.head())
+
+    #print("\nDeclarative Constraints:")
+    #print(declarative_constraints)
+    # Display declarative constraints so that user can select which constraints to use for the analysis.
+
+    if declarative_constraints:
+        print("\nDeclarative Constraints:")
+        for constraint in declarative_constraints:
+            if constraint.get("type")=="Unary":
+             print(f"{constraint['id']}. {constraint['template']}({{{', '.join(constraint['activity'])}}}) ")
+            else:
+             print(f"{constraint['id']}. {constraint['template']}({{{', '.join(constraint['source'])}}}, {{{', '.join(constraint['target'])}}}) ")
+
+    while True:
+        selected_constraint=input("\nEnter the ID of the constraint you want to use for analysis (or type 'exit' to quit): ")
+        if selected_constraint.lower() == 'exit':
+            print("Exiting the program.")
+            break
+        # Check if the selected constraint ID is valid
+        if any(constraint['id'] == selected_constraint for constraint in declarative_constraints):
+            print(f"You selected constraint ID: {selected_constraint}")
+            # Here you can add code to perform analysis based on the selected constraint
+            
+        else:
+            print("Invalid constraint ID. Please try again.")            
+
+       
+
+   
---
+++ load_data.py
@@ -0,0 +1,71 @@
+# Loading events logs from XES file and declarative constraints from JSON file
+
+import json
+import pm4py
+import pandas as pd
+
+def load_events_logs(file_path):
+    """
+    Load events logs from a specified file path.
+
+    Args:
+        file_path (str): The path to the events logs file.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the event logs.
+    """
+    # read XES file
+    """df = pm4py.read_xes("BPIC19_3way_IbeforeGR_standardPO_complete.xes")
+    # sort:
+    # 1) by case identifier (ascending)
+    # 2) by event timestamp (ascending within each case)
+    df = df.sort_values(
+        by=["case:concept:name", "time:timestamp"],
+        ascending=[True, True]
+    )
+
+    # if df is empty, raise an error
+    if df.empty:
+        raise ValueError("The events logs dataframe is empty. Please check the input file.")""" 
+    
+    # To save processing time, we read the preprocessed CSV file instead of the XES file. The CSV file is assumed to be in the same directory as this script.
+    df = pd.read_csv("bpic19_invoice_before_gr_all_cases.csv")
+
+     # export everything to CSV
+    #df.to_csv("bpic19_invoice_before_gr_all_cases.csv", index=False)
+
+    #print("Exported all cases to bpic19_invoice_before_gr_all_cases.csv")
+
+    return df
+
+def load_declarative_constraints(file_path):
+    """
+    Load declarative constraints from a specified file path.
+
+    Args:
+        file_path (str): The path to the declarative constraints file 
+
+    Returns:
+        list: A list of declarative constraints.
+    """
+    # read the declarative_constraints.json file and return the constraints as a list of strings
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data["constraints"]
+
+def load_data_function(events_logs_path, declarative_constraints_path):
+    """
+    Load events logs and declarative constraints from specified file paths.
+
+    Args:
+        events_logs_path (str): The path to the events logs file.
+        declarative_constraints_path (str): The path to the declarative constraints file.
+
+    Returns:
+        tuple: A tuple containing the DataFrame of event logs and the list of declarative constraints.
+    """
+    event_logs = load_events_logs(events_logs_path)
+    declarative_constraints = load_declarative_constraints(declarative_constraints_path)
+
+    return event_logs, declarative_constraints
